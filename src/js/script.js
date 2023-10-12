e615e33ca8c305876175f4a24766ecd438b11255
Naoko705310 <naokos79@gmail.com>
2023-10-12 22:25:42 +0700

    product__swiper高さ調節（要修正）

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,47 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
+  // 製品一覧：スワイパー
+  const swiper = new Swiper('.js-products-swiper', {
+    // Optional parameters
+    loop: true,
+    speed: 4000,
+    // autoplay: {
+    //   delay: 3000,
+    // },
+    allowTouchMove: true,
+    spaceBetween: 24,
+    breakpoints: {
+      768: {
+        slidesPerView: 4, // 中央に3枚のカードを表示
+        spaceBetween: 40, // カード間のスペース
+        centeredSlides: true, // 中央にカードを配置
+      }
+    },
 
-});
+
+
+
+
+
+
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });//products__swiper閉じタグ
+
+
+}); //JS閉じタグ
