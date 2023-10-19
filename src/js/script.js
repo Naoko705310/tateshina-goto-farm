ded5b3e0171dc45c5127f05f47a6165d4b8f1215
Naoko705310 <naokos79@gmail.com>
2023-10-19 10:46:41 +0700

    products スワイパー修正（やり直し）

--- src/js/script.js
+++ src/js/script.js
@@ -44,38 +44,24 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     }
   });
 
-  // 製品一覧：スワイパー
+  // 製品一覧：スワイパー **********
   const swiper = new Swiper('.js-products-swiper', {
-    // Optional parameters
     loop: true,
     speed: 4000,
     autoplay: {
       delay: 3000,
     },
     allowTouchMove: true,
+    slidesPerView: 1.5, 
     spaceBetween: 24,
+    centeredSlides: true,
     breakpoints: {
       768: {
-        slidesPerView: 4, // 中央に3枚のカードを表示
-        spaceBetween: 40, // カード間のスペース
-        centeredSlides: true, // 中央にカードを配置
+        slidesPerView: 4, 
+      },
+      1440: {
+        slidesPerView: 6, 
       }
-    },
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
     },
   });//products__swiper閉じタグ
 
